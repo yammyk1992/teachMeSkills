0838eea1a0051522419613118642b81bf2e6b542
Artem Shcherban <yammyk1992@gmail.com>
2022-04-16 14:07:32 +0300

    add exceptions

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+__author__ = 'Artem Shcherban'
+
+
+class ValidationError(Exception):
+    """Класс ошибки валидации данных."""
+
+
+class RegistrationError(Exception):
+    """Класс ошибки """
+
+
+class AuthorizationError(Exception):
+    """Ошибка авторизации"""
