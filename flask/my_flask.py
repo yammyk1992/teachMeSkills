67e392f495c9588b386cdb1e26bc2c60bd8d6a30
Artem Shcherban <yammyk1992@gmail.com>
2022-04-25 18:50:27 +0300

    add exceptions

---
+++ flask/my_flask.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+@app.route("/about")
+def about():
+    return "<h1>О сайте </h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
